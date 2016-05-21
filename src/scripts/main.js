8433fdded6e80d43f4c777f12c92f121d9a8d5ce
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-05-21 02:42:08 +0000

    Create basic logo demo

---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+// Polyfills
+// import 'es6-promise'
+// import 'whatwg-fetch'
+
+import analytics from './modules/analytics'
+import domready from './modules/domready'
+import global from './modules/global'
+// import initRouter from './init/router'
+
+analytics('UA-77889324-2')
+// initRouter()
+
+domready(() => {
+  setTimeout(() => global.setAttr('intro', '1'), 1000)
+  setTimeout(() => global.setAttr('intro', '2'), 2000)
+  setTimeout(() => global.setAttr('intro', '3'), 3000)
+  setTimeout(() => global.setAttr('intro', '4'), 4000)
+  setTimeout(() => global.setAttr('intro', '5'), 5000)
+  setTimeout(() => global.setAttr('intro', '6'), 9000)
+  setTimeout(() => global.setAttr('intro', '7'), 11000)
+  setTimeout(() => global.setAttr('intro', '8'), 15000)
+  setTimeout(() => global.setAttr('intro', '9'), 19000)
+  setTimeout(() => global.setAttr('intro', '10'), 23000)
+})
